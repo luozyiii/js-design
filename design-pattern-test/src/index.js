453afd288c7136bd7780dc7c7488730b4157165e
罗智毅 <zhiyiluo@drgou.com>
2021-11-23 12:37:28 +0800

    装饰器模式

--- design-pattern-test/src/index.js
+++ design-pattern-test/src/index.js
@@ -1,27 +1,9 @@
-function loadImg(src) {
-    let promise = new Promise(function(resolve, reject){
-        let img = document.createElement('img');
-        img.onload = function() {
-            resolve(img)
-        }
-        img.onerror = function() {
-            reject('图片加载失败')
-        }
-        img.src = src
-    });
-    return promise;
+import { readonly } from 'core-decorators';
+ 
+class Meal {
+  @readonly
+  entree = 'steak';
 }
-
-let src = 'https://p26-passport.byteacctimg.com/img/user-avatar/4d0a1fbdc3d957f62ecde912c80082b0~300x300.image'
-let result = loadImg(src);
-
-result.then(function(img){
-    // part 1
-    console.log(`width: ${img.width}`)
-    return img
-}).then(function(img){
-    // part 2
-    console.log(`height: ${img.height}`)
-}).catch(function(err){
-    console.log(err)
-})
+ 
+var dinner = new Meal();
+dinner.entree = 'salmon';
