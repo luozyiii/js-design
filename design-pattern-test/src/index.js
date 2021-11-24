f033ce423c5574879c78ee52b3dd2363a903c879
罗智毅 <zhiyiluo@drgou.com>
2021-11-24 18:19:07 +0800

    状态模式

--- design-pattern-test/src/index.js
+++ design-pattern-test/src/index.js
@@ -1,42 +1,81 @@
 import StateMachine from 'javascript-state-machine'
-import $ from 'jquery'
 
 // 初始化状态机模型
 let fsm = new StateMachine({
-  init: '收藏',
+  init: 'pending', // 初始化状态
   transitions: [
-    { name: 'doStore',     from: '收藏',  to: '取消收藏' },
-    { name: 'deleteStore',   from: '取消收藏', to: '收藏'  },
+    { 
+      name: 'resolve',
+      from: 'pending',
+      to: 'fullfilled'
+    },
+    { 
+      name: 'reject',
+      from: 'pending',
+      to: 'rejected'
+    },
   ],
   methods: {
-    // 监听执行收藏
-    onDoStore: function() {
-      alert('收藏成功')
-      updateText()
+    // 监听 resolve
+    onResolve: function(state, data) {
+      // state - 当前状态机实例； data - fsm.resolve(xxx) 传递的参数
+      console.log(state, data)
+      data.successList.forEach(fn => fn())
     },
-    // 监听取消收藏
-    onDeleteStore: function() {
-      alert('已经取消收藏')
-      updateText()
+    // 监听 reject
+    onReject: function(state, data) {
+      // state - 当前状态机实例； data - fsm.reject(xxx) 传递的参数
+      data.successList.failList(fn => fn())
     },
   }
 })
 
-let $btn = $('#btn1')
+// 定义 Promise
+class MyPromise {
+  constructor(fn) {
+    this.successList = []
+    this.failList = []
 
-// 按钮点击事件
-$btn.click(function() {
-  if(fsm.is('收藏')) {
-    fsm.doStore()
-  }else {
-    fsm.deleteStore()
+    fn(function (){
+      // resolve 函数
+      fsm.resolve(this)
+    }, function () {
+      // reject 函数
+      fsm.reject(this)
+    })
   }
-})
 
-// 更新按钮的文案
-function updateText() {
-  $btn.text(fsm.state)
+  then(succesFn, failFn) {
+    this.successList.push(succesFn)
+    this.failList.push(failFn)
+  }
 }
 
-// 初始化文案
-updateText()
+// 测试代码
+function loadImg(src) {
+  const promise = new MyPromise(function(resolve, reject){
+    let img = document.createElement('img')
+    img.onload = function (){
+      resolve(img)
+    }
+    img.onerror = function () {
+      reject()
+    }
+    img.src = src
+  })
+  return promise
+}
+
+let src = 'https://p26-passport.byteacctimg.com/img/user-avatar/4d0a1fbdc3d957f62ecde912c80082b0~300x300.image'
+let result = loadImg(src);
+
+result.then(function() {
+  console.log('ok1')
+}, function() {
+  console.log('fail1')
+})
+result.then(function() {
+  console.log('ok2')
+}, function() {
+  console.log('fail2')
+})
